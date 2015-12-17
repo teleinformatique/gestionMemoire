65988000e0eeebefe70345e1dd13cac8eaebd17c
Khadim Sarr <khadimsarr@outlook.com>
2015-12-17 18:26:12 +0000

    creation template InsertMemoire

---
+++ app/client/templates/insert_memoire/insert_memoire.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* InsertMemoire: Event Handlers */
+/*****************************************************************************/
+Template.InsertMemoire.events({
+});
+
+/*****************************************************************************/
+/* InsertMemoire: Helpers */
+/*****************************************************************************/
+Template.InsertMemoire.helpers({
+});
+
+/*****************************************************************************/
+/* InsertMemoire: Lifecycle Hooks */
+/*****************************************************************************/
+Template.InsertMemoire.onCreated(function () {
+});
+
+Template.InsertMemoire.onRendered(function () {
+});
+
+Template.InsertMemoire.onDestroyed(function () {
+});
